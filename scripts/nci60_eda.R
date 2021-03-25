e80a8e504d63290f6c3176a8b1a6651b7302533d
Karl Kumbier <kkumbier@berkeley.edu>
2021-03-25 13:42:58 -0700

    update to RF models, add null performance with random cell line selection

--- scripts/nci60_eda.R
+++ scripts/nci60_eda.R
@@ -9,8 +9,11 @@ library(caret)
 library(viridis)
 library(parallel)
 library(iRF)
+library(superheat)
 setwd('~/github/cancer_translator/')
 source('scripts/utilities.R')
+
+train.prop <- 0.8
 
 #' The NCI60 data, describing bioactivity for 60 CCLs are available through 
 #' rcellminer. Below we consider a subset of drug classes from the ORACL paper,
@@ -28,6 +31,10 @@ source('scripts/utilities.R')
 class.key <- c(
   MT='Tu',
   DNA='(TOP2|Ds|Df|DHFR|AM|GARTF)',
+  VEGFR='VEGFR',
+  A7='A7',
+  YK='YK',
+  PIK3='PIK3',
   HDAC='HDAC',
   mTOR='MTOR',
   AKT='AKT'
@@ -36,6 +43,14 @@ class.key <- c(
 # Read in MOA data from NCI60
 nsc.set <- unique(c(getMoaToCompounds(), recursive=TRUE))
 moa <- getMoaStr(nsc.set)
+
+moa.split <- unlist(str_split(moa, ','))
+moa.table <- data.frame(MoA=moa.split) %>%
+  group_by(MoA) %>%
+  summarize(Count=n()) %>%
+  arrange(desc(Count))
+
+write.csv(file='~/Desktop/moa_table.csv', moa.table, row.names=F, quote=F)
 
 # Map MOA to compound class
 class <- sapply(class.key, function(k) str_detect(moa, k))
@@ -51,17 +66,36 @@ x.activity <- getDrugActivityData(nsc.set)
 
 # Impute NAs as median within cell line
 x.activity <- apply(x.activity, MAR=2, median_impute)
+print(dim(x.activity))
+
+# Upsample for class imbalance
+# If upsampling, we need to split train/test to not including training samples 
+# in test set. Discuss up/down sampling - should models be giving equal weight 
+# across drug classes?
+xbalanced <- downSample(x.activity, class.id)
+class.id <- xbalanced$Class
+x.activity <- select(xbalanced, -Class)
+print(dim(x.activity))
 
 #' We visualize bioactivity patterns within our compound set below. Heatmap rows 
 #' correspond to different compounds and columns cell lines. Compound classes 
 #' cluster relative to PC1 and PC2 but show high variability.
 #+ bioactivity_heatmap, fig.height=10, fig.width=15
 # Bioactivity heatmap visualization
-col.pal <- RColorBrewer::brewer.pal(length(class.key), 'Set1')
-heatmap(x.activity, 
-        col=inferno(100),
-        RowSideColors=col.pal[as.numeric(class.id)])
-legend('topleft', legend=names(class.key), fill=col.pal)
+xplot <- apply(x.activity, MAR=2, rank)
+type <- as.factor(str_remove_all(colnames(xplot), ':.*'))
+type.pal <- scales::hue_pal()(length(unique(type)))
+
+superheat(xplot, 
+          membership.rows=class.id,
+          membership.cols=type,
+          pretty.order.rows=TRUE,
+          pretty.order.cols=TRUE,
+          heat.pal=inferno(100),
+          bottom.label='variable',
+          bottom.label.text.col=type.pal[type],
+          bottom.label.size=0.5,
+          bottom.label.text.angle=90)
 
 # Bioactivity PCA plot visualization
 xpca <- prcomp(apply(x.activity, MAR=2, rank))
@@ -80,51 +114,28 @@ data.frame(xpca$x, Class=class.id) %>%
 #' ## Predictive modeling
 #' Below, we consider the problem of predicting which compound class a given 
 #' compound belongs to, and how many cell lines are required for prediction.
-#+ supervised_modeling, warning=FALSE, fig.height=10, fig.width=15
+#+ supervised_modeling, warning=FALSE
 y <- as.numeric(class.id) - 1
 x <- x.activity
-ncell <- 1:20
+ncell <- 2:10
+n.replicate <- 50
 
-fits <- mclapply(1:50, function(i) {
+fits <- mclapply(1:n.replicate, function(i) {
   set.seed(i)
   
   # Generate sample split
-  id.train <- createDataPartition(y, p=0.8)$Resample1
+  id.train <- createDataPartition(y, p=train.prop)$Resample1
   id.test <- setdiff(1:nrow(x), id.train)
   
-  # Fit lasso to predict moa from bioactivity
-  fit <- cv.glmnet(x=x[id.train,], y=y[id.train], family='multinomial')
-  out <- lapply(ncell, model_accuracy, fit=fit, x=x[id.test,], y=y[id.test])
-
-  accuracy <- sapply(out, function(z) z$accuracy)
-  betas <- sapply(out, function(z) z$betas)
-  ypred <- data.frame(ID=as.factor(id.test), sapply(out, function(z) z$ypred))
-  
-  return(list(accuracy=accuracy, betas=betas, ypred=ypred))
+  # Fit RF to predict moa from bioactivity
+  out <- irf_fit(x, y, id.train, id.test, ncell)
+  return(list(accuracy=out$accuracy, selected=out$selected))
 }, mc.cores=10)
 
-
 accuracy <- lapply(fits, function(f) as.matrix(f$accuracy))
-betas <- lapply(fits, function(f) f$betas)
-ypred <- rbindlist(lapply(fits, function(f) f$ypred)) %>%
-  group_by(ID) %>%
-  summarize_if(is.numeric, mean) %>%
-  arrange(match(ID, 1:nrow(x)))
+selected <- lapply(fits, function(f) f$selected)
 
-reshape2::melt(betas) %>%
-  rename(CellLine=Var1, Ncell=Var2, Rep=L1) %>%
-  mutate(Ncell=ncell[Ncell]) %>%
-  group_by(CellLine) %>%
-  mutate(Drop=all(value == 0)) %>%
-  filter(!Drop) %>%
-  ggplot(aes(x=reorder(CellLine, value, median), y=value)) +
-  facet_wrap(~Ncell) +
-  geom_boxplot(aes(fill=CellLine)) +
-  coord_flip() +
-  theme_bw() +
-  theme(legend.position='none') +
-  ggtitle('Distribution of model coefficients')
-
+#+ supervised_modeling_accuracy, warning=FALSE, fig.height=10, fig.width=10
 reshape2::melt(accuracy) %>%
   mutate(Ncell=as.factor(ncell[Var1])) %>%
   ggplot(aes(x=Ncell, y=value)) +
@@ -133,86 +144,66 @@ reshape2::melt(accuracy) %>%
   ylab("Top-1 classification accuracy") +
   ggtitle('Model accuracy by cell lines used')
 
-# Plot PCA distribution of compounds from select cell lines
+#+ supervised_modeling_beta, warning=FALSE, fig.height=18, fig.width=12
+reshape2::melt(selected) %>%
+  rename(CellLine=Var1, Ncell=Var2, Rep=L1) %>%
+  mutate(Ncell=ncell[Ncell]) %>%
+  group_by(CellLine, Ncell) %>%
+  summarize(Proportion=mean(value)) %>%
+  filter(Proportion != 0) %>%
+  ggplot(aes(x=reorder(CellLine, Proportion), y=Proportion)) +
+  facet_wrap(~Ncell) +
+  geom_bar(stat='identity', aes(fill=CellLine)) +
+  coord_flip() +
+  theme_bw() +
+  theme(legend.position='none') +
+  ggtitle('Cell line selection proportion')
+
+#+ supervised_modeling_heatmap, warning=FALSE, fig.height=12, fig.width=12
+# Plot distribution of compounds from select cell lines
 ncell.plot <- 5
-cell.select <- reshape2::melt(betas) %>%
-  filter(L1 == ncell.plot) %>%
-  group_by(Var1) %>%
-  summarize(value=mean(value)) %>%
-  top_n(ncell.plot, value)
+cell.select <- reshape2::melt(selected) %>%
+  rename(CellLine=Var1, Ncell=Var2) %>%
+  mutate(Ncell=ncell[Ncell])
+  
+cell.select.k <- cell.select %>%
+  filter(Ncell == ncell.plot) %>%
+  group_by(CellLine, Ncell) %>%
+  summarize(Proportion=mean(value)) %>%
+  ungroup() %>%
+  top_n(ncell.plot, Proportion)
 
-xpca <- prcomp(apply(x[,cell.select$Var1], MAR=2, rank))
-data.frame(PctVar=cumsum(xpca$sdev ^ 2) / sum(xpca$sdev ^ 2)) %>%
-  mutate(NPC=1:n()) %>%
-  ggplot(aes(x=NPC, y=PctVar)) +
-  geom_point() +
-  geom_line() +
-  theme_bw()
+selected <- c(as.character(cell.select.k$CellLine), 'LC:A549/ATCC')
+xplot <- apply(x[,selected], MAR=2, rank)
+id.highlight <- colnames(xplot) == 'LC:A549/ATCC'
 
-data.frame(xpca$x, Class=class.id) %>%
-  ggplot(aes(x=PC1, y=PC2, col=Class)) +
-  geom_point() +
-  theme_bw()
-
-xplot <- apply(x[,cell.select$Var1], MAR=2, rank)
-yplot <- select(ypred, one_of(str_c('X', ncell.plot)))[[1]]
 superheat(xplot, 
           membership.rows=class.id,
-          yr=yplot,
-          yr.plot.size=1,
           pretty.order.rows=TRUE,
           pretty.order.cols=TRUE,
           heat.pal=inferno(100),
-          bottom.label.text.angle=90)
+          bottom.label.size=1,
+          bottom.label.text.angle=90,
+          bottom.label.text.col=ifelse(id.highlight, 'red', 'black'))
 
-# Compute random forest performance using select cell lines
-xcell <- x[,cell.select$Var1]
-rf.accuracy <- mclapply(1:1000, function(i) {
-  set.seed(i)
-  
-  # Generate sample split
-  id.train <- createDataPartition(y, p=0.8)$Resample1
-  id.test <- setdiff(1:nrow(x), id.train)
-  
-  # Fit lasso to predict moa from bioactivity
-  fit <- iRF(x=xcell[id.train,], y=as.factor(y[id.train]), type='ranger', n.iter=1)
-  ypred <- predict(fit$rf.list, xcell[id.test,])
-  accuracy <- mean(ypred$predictions == y[id.test])
-  
-  
-  return(accuracy)
-}, mc.cores=10)
-
-data.frame(Accuracy=unlist(rf.accuracy)) %>%
-  ggplot(aes(x=Accuracy)) +
-  geom_histogram(bins=15) +
-  theme_bw() +
-  ggtitle('RF prediction accuracy', str_c(ncell.plot, ' cell lines'))
-
-#' ## Null predictive modeling
+#' ## Null predictive modeling - permuted responses
 #' To assess performance against random chance, we repeat the experiment above 
 #' using randomly permuted response labels.
 #+ random_permute, fig.height=10, fig.width=15
-fits.null <- mclapply(1:50, function(i) {
+fits.null <- mclapply(1:n.replicate, function(i) {
   set.seed(i)
   
   # Generate sample split
-  id.train <- createDataPartition(y, p=0.8)$Resample1
+  id.train <- createDataPartition(y, p=train.prop)$Resample1
   id.test <- setdiff(1:nrow(x), id.train)
   
   # Fit lasso to predict moa from bioactivity
   yy <- sample(y)
-  fit <- cv.glmnet(x=x[id.train,], y=yy[id.train], family='multinomial')
-  out <- lapply(ncell, model_accuracy, fit=fit, x=x[id.test,], y=yy[id.test])
-  
-  accuracy <- sapply(out, function(z) z$accuracy)
-  betas <- sapply(out, function(z) z$betas)
-  
-  return(list(accuracy=accuracy, betas=betas))
+  out <- irf_fit(x, yy, id.train, id.test, ncell)
+  return(out$accuracy)
 }, mc.cores=10)
 
-accuracy <- lapply(fits.null, function(f) as.matrix(f$accuracy))
-
+accuracy <- lapply(fits.null, as.matrix)
 reshape2::melt(accuracy) %>%
   mutate(Ncell=as.factor(ncell[Var1])) %>%
   ggplot(aes(x=Ncell, y=value)) +
@@ -220,3 +211,35 @@ reshape2::melt(accuracy) %>%
   theme_bw() +
   ylab("Top-1 classification accuracy") +
   ggtitle('Model accuracy by cell lines used - permuted responses')
+
+#' ## Null predictive modeling - random cell lines
+#' To assess the performance against randomly selected cell lines, we repeat 
+#' the experiment above using a random sample of $k$ cell lines for $k = 1, ..., 
+#' 20$.
+#+ random cell line
+# Drop single cell due to modeling constraints
+fits.null <- mclapply(1:n.replicate, function(i) {
+  set.seed(i)
+  
+  # Generate sample split
+  id.train <- createDataPartition(y, p=train.prop)$Resample1
+  id.test <- setdiff(1:nrow(x), id.train)
+  
+  # Fit RF to predict moa from bioactivity
+  out.null <- irf_fit(x, y, id.train, id.test, ncell, null.cells=TRUE)
+  out.opt <-  irf_fit(x, y, id.train, id.test, ncell)
+  
+  return(rbind(out.null$accuracy, out.opt$accuracy))
+}, mc.cores=10)
+
+
+reshape2::melt(fits.null) %>%
+  rename(Selection=Var1, Ncell=Var2, Rep=L1) %>%
+  mutate(Selection=ifelse(Selection == 1, 'Random', 'Optimal')) %>%
+  mutate(Ncell=Ncell + 1) %>%
+  mutate(Ncell=as.factor(Ncell)) %>%
+  group_by(Ncell, Rep) %>%
+  summarize(Diff=value[Selection == 'Optimal'] - value[Selection == 'Random']) %>%
+  ggplot(aes(x=Ncell, y=Diff)) +
+  geom_boxplot(fill='#0388D1') +
+  theme_bw()
--- scripts/utilities.R
+++ scripts/utilities.R
@@ -6,11 +6,59 @@ median_impute <- function(x) {
 
 abind3 <- function(...) abind::abind(..., along=3)
 
+irf_fit <- function(x, y, train.id, test.id=NULL, k=2:20, null.cells=FALSE) {
+  # Fit random forest model constrained to a select number of cell lines
+  if (is.null(test.id)) test.id <- setdiff(1:nrow(x), train.id)
+  
+  # Fit initial model to assess variable importance
+  fit <- iRF(x=x[train.id,], 
+             y=as.factor(y)[train.id], 
+             type='ranger', 
+             n.iter=2)
+  
+  importance <- fit$rf.list$variable.importance
+  
+  # Fit models with constrained number of cell lines
+  out <- lapply(k, function(kk) {
+    # Select optimal cell lines
+    if (!null.cells) id.select <- tail(sort(importance), kk)
+    if (null.cells) id.select <- sample(importance, kk)
+    xselect <- x[,names(id.select)]
+    
+    # Fit model constraining # of cell lines
+    fitk <- iRF(x=xselect[train.id,], 
+                y=as.factor(y)[train.id], 
+                type='ranger', 
+                n.iter=1)
+    
+    # Evaluate model performance
+    ypred <- predict(fitk$rf.list, xselect[test.id,])
+    accuracy <- mean(ypred$predictions == y[test.id])
+    
+    # Return selected cell lines
+    selected <- colnames(x) %in% names(id.select)
+    names(selected) <- colnames(x)
+    return(list(accuracy=accuracy, selected=selected))
+  })
+  
+  accuracy <- sapply(out, function(z) z$accuracy)
+  selected <- sapply(out, function(z) z$selected)
+  return(list(selected=selected, accuracy=accuracy))
+}
+
+
+
+
+
+################################################################################
+# Old utility functions
+################################################################################
 model_accuracy <- function(fit, x, y, smax=5) {
   # Generate model predictions given a maximum number of cell lines
   
   # Evaluate number of cell lines used for each model (lambda value)
-  betas <- lapply(coef(fit$glmnet.fit), function(b) as.matrix(b[-1,]))
+  
+  betas <- lapply(coef(fit), function(b) as.matrix(b[-1,]))
   betas <- do.call(abind3, betas)
   
   # Collapse betas and identify maximal model under given constraint
@@ -19,11 +67,12 @@ model_accuracy <- function(fit, x, y, smax=5) {
   idmax <- max(which(s <= smax))
   
   # Get model predictions
-  ypred <- predict(fit$glmnet.fit, x, type='response')
+  ypred <- predict(fit, x, type='response')
   ypred <- ypred[,,idmax]
   
   # Normalize predictions for class imblanace
   ypred.true <- mapply(function(ii, iy) ypred[ii, iy + 1], 1:length(y), y)
+  ypred <- apply(ypred, MAR=2, function(z) z / mean(z))
   ypred.class <- apply(ypred, MAR=1, which.max) - 1
   
   # Return beta as average absolute coefficient
