a8ac53e0c8d7b822b030aca2db021e10d0aff277
Karl Kumbier <kkumbier@berkeley.edu>
2021-07-15 06:18:13 -0700

    screen analytsis

--- scripts/screen_qc.R
+++ scripts/screen_qc.R
@@ -1,10 +1,12 @@
-#+ setup, echo=FALSE
+#+ setup, echo=FALSE, message=FALSE, warning=FALSE
 library(data.table)
 library(tidyverse)
 library(rprofiler)
 library(superheat)
 library(tidytext)
 library(parallel)
+library(iRF)
+library(caret)
 
 ################################################################################
 # Setup
@@ -42,7 +44,11 @@ if (intensity.normalize) {
   x <- rbindlist(x)
 }
 
-#+ eda, fig.height=8, fig.width=8
+#' #QC
+#' The figures below report the distribution of cell counts by plate/cell line.
+#' We drop plate `2021018028` which appears to have a strange count distribution 
+#' relative to other A549 plates.
+#+ eda, fig.height=8, fig.width=12
 ################################################################################
 # Simple EDA - counts and intensity
 ################################################################################
@@ -71,6 +77,12 @@ filter(x, Control) %>%
 # Drop plate 2021018028 - strange cell count distribution
 x <- filter(x, PlateID != '2021018028')
 
+#' Preliminary EDA indicated strong "column" effects, particularly for intensity 
+#' features. We normalize all features relative to well position by regressing
+#' features on column ID — i.e. we remove the portion of a feature that can be
+#' explained by a wells column position. Plots below report the distribution of 
+#' features for DMSO wells after normalization.
+#+ superheat, fig.height=12, fig.width=18
 # Feature distribution plots
 xcontrol <- filter(x, Control)
 plate.id <- xcontrol$PlateID
@@ -127,7 +139,12 @@ filter(x, Control) %>%
   theme(axis.text.x=element_text(angle=90)) +
   ylab('KS')
 
-#+ 
+#' # PCA analysis
+#' To qualitatively assess the feature distribution of different compound 
+#' classes we plot PCA projections grouped by various metadata features. Broadly
+#' speaking, the primary sources of variation in the data correspond to compound
+#' categories.
+#+ pca, fig.height=6, fig.width=12
 ################################################################################
 # Full dataset PCA
 ################################################################################
@@ -142,16 +159,8 @@ data.frame(PctVar=cumsum(xpca$sdev ^ 2) / sum(xpca$sdev ^ 2)) %>%
   geom_line() +
   theme_bw()
 
-pc.list <- list(c('PC1', 'PC2'), c('PC3', 'PC4'), c('PC5', 'PC6'))
+pc.list <- list(c('PC1', 'PC2'), c('PC3', 'PC4'))
 for (pcs in pc.list) {
-  
-  p <- ggplot(x, aes_string(x=pcs[1], y=pcs[2])) +
-    geom_point(aes(col=log(NCells), shape=Control), alpha=0.5) +
-    facet_wrap(~PlateID) +
-    theme_bw() +
-    scale_color_gradientn(colors=viridis::inferno(10)[1:9]) +
-    ggtitle('Cell counts by plate')
-  plot(p)
   
   p <- filter(x, !str_detect(Compound_Usage, '(query|reference)')) %>%
     ggplot(aes_string(x=pcs[1], y=pcs[2])) +
@@ -161,31 +170,54 @@ for (pcs in pc.list) {
     facet_wrap(~Cell_Line)
   plot(p)
   
-  p <- filter(x, Compound_Usage == 'reference_cpd') %>%
+  p <- filter(x, Compound_Usage == 'reference_cpd' | Compound_ID == 'DMSO') %>%
+    mutate(DMSO=Compound_ID == 'DMSO') %>%
     ggplot(aes_string(x=pcs[1], y=pcs[2])) +
-    geom_point(aes(col=Compound_Category), alpha=0.5) +
+    geom_point(aes(col=Compound_Category, shape=DMSO, alpha=DMSO)) +
     theme_bw() +
-    ggtitle('Positive v. negative controls') +
+    scale_shape_manual(values=c(19, 1)) +
+    scale_alpha_manual(values=c(0.6, 0.3)) +
+    ggtitle('Compound category') +
     facet_wrap(~Cell_Line)
   plot(p)
   
-  p <- filter(x, Compound_Usage == 'reference_cpd') %>%
+  p <- filter(x, Compound_Usage == 'reference_cpd' | Compound_ID == 'DMSO') %>%
+    mutate(DMSO=Compound_ID == 'DMSO') %>%
     ggplot(aes_string(x=pcs[1], y=pcs[2])) +
-    geom_point(aes(col=Pathway), alpha=0.5) +
+    geom_point(aes(col=Pathway, shape=DMSO, alpha=DMSO)) +
     theme_bw() +
-    ggtitle('Positive v. negative controls') +
+    scale_shape_manual(values=c(19, 1)) +
+    scale_alpha_manual(values=c(0.6, 0.3)) +
+    ggtitle('Pathway') +
     facet_wrap(~Cell_Line)
   plot(p)
   
-  p <- filter(x, Compound_Usage == 'reference_cpd') %>%
+  p <- filter(x, Compound_Usage == 'reference_cpd' | Compound_ID == 'DMSO') %>%
+    mutate(DMSO=Compound_ID == 'DMSO') %>%
+    mutate(Target=ifelse(Compound_ID == 'DMSO', 'DMSO', Target)) %>%
     ggplot(aes_string(x=pcs[1], y=pcs[2])) +
-    geom_point(aes(col=Target), alpha=0.5) +
+    geom_point(aes(col=Target, shape=DMSO, alpha=DMSO)) +
     theme_bw() +
-    ggtitle('Positive v. negative controls') +
+    scale_shape_manual(values=c(19, 1)) +
+    scale_alpha_manual(values=c(0.6, 0.3)) +
+    ggtitle('Target') +
     facet_wrap(~Cell_Line)
   plot(p)
 }
 
+#' # Bioactivity filtering
+#' Before training compound classifiers, we filter out compounds/doses that
+#' cannot be distinguished from DMSO. Specifically, we (i) take a subsample
+#' of DMSO wells (ii) compute the center of the DMSO point cloud by taking
+#' average feature values across each well in the subsample (iii) compute the
+#' l2 distance between each DMSO well in the subsample and the DMSO point cloud 
+#' center (iv) define the DMSO maximal distance as the maximum distance (from 
+#' iii) over all subsampled wells.
+#' 
+#' We then ask whether a given well (any compound) is further from the DMSO
+#' point cloud center than the maximal DMSO distance. Repeating this process
+#' across many subsamples allows us to generate a bioactivity p-value.
+#+ bioactivity
 ################################################################################
 # Bioactivity
 ################################################################################
@@ -201,83 +233,117 @@ xdist <- mclapply(unique(x$PlateID), function(p) {
 
 xdist <- rbindlist(xdist)
 
-# Filter to compounds that have the same dose schedule for all cell lines
-xdist.summary <- select(xdist, Cell_Line, Compound_ID, Dose_Category) %>%
-  distinct() %>%
-  group_by(Compound_ID, Cell_Line) %>%
-  summarize(Dose=str_c(sort(Dose_Category), collapse='_'), .groups='drop') %>%
-  group_by(Compound_ID) %>%
-  mutate(Count=n()) %>%
-  filter(Count == 3) %>%
-  filter(Dose=='1_2_3_4_5')
-  #filter(length(unique(Dose)) == 1)
-  
-xdist <- filter(xdist, Compound_ID %in% xdist.summary$Compound_ID)
-#' # Bioactivity by compound category/target
-#' To assess differential compound bioactivity by cell lines, we first compute 
-#' the average distance between each `Compound_Category`/`Target` and DMSO center 
-#' (separately within each cell line). **Note:** this average is taken across 
-#' all dose categories. We restrict to compounds measured in all cell lines.
-#+ distance_category, fig.heigh=6, fig.width=12
-# Average distance by category
-reference.category <- filter(xdist, Compound_Usage == 'reference_cpd')$Compound_Category
-x.category <- group_by(xdist, Compound_Category, Cell_Line) %>%
-  summarize(DistNorm=mean(DistNorm), .groups='drop') %>%
-  group_by(Compound_Category) %>%
-  mutate(Count=n()) %>%
-  filter(Count == 3)
+# Filter to reference compound doses that are bioactive
+xdist.select <- filter(xdist, Compound_Usage == 'reference_cpd') %>%
+  filter(pval <= 0.05) %>%
+  mutate(Compound_Dose=str_c(Compound_ID, ', ', Dose_Category))
 
-xplot <- matrix(x.category$DistNorm, nrow=3)
-colnames(xplot) <- unique(x.category$Compound_Category)
-rownames(xplot) <- unique(x.category$Cell_Line)
-
-superheat(xplot,
-          pretty.order.rows=TRUE,
-          pretty.order.cols=TRUE,
-          heat.pal=viridis::inferno(10),
-          bottom.label.text.angle=90,
-          bottom.label.size=0.5,
-          title='Bioactivity by cell line, compound category')
-
-
-# Average distance by target
-reference.target <- filter(xdist, Compound_Usage == 'reference_cpd')$Target
-x.target <- group_by(xdist, Target, Cell_Line) %>%
-  summarize(DistNorm=mean(DistNorm), .groups='drop') %>%
-  group_by(Target) %>%
-  mutate(Count=n()) %>%
-  filter(Count == 3) %>%
-  filter(!is.na(Target))
-
-xplot <- matrix(x.target$DistNorm, nrow=3)
-colnames(xplot) <- unique(x.target$Target)
-rownames(xplot) <- unique(x.target$Cell_Line)
-
-superheat(xplot,
-          pretty.order.rows=TRUE,
-          pretty.order.cols=TRUE,
-          heat.pal=viridis::inferno(10),
-          bottom.label.text.angle=90,
-          bottom.label.size=0.5)
-
-#' ## Differential phenotypes
-#' Below we consider examples of compopunds with targets that show differential 
-#' activity across cell lines (above):
-#' 1. Camptothecin (Topoisomerase) — active in A549, OVCAR4
-#' 2. Gemcitabine (Nucleic Acids) — active in OVCAR, 786-0
-#' 3. Pemetrexed (DHFR. TS. GARFT) — active in 786-0
-#+ differential_activity, fig.height=8, fig.width=12
-plot_compound(x, 'Camptothecin')
-plot_compound(x, 'Gemcitabine')
-plot_compound(x, 'Pemetrexed')
-
-# TODO: classification, treat different doses independently of one another
-
+#' # Modeling
+#' For each cell line, we train classifiers to predict compound category from
+#' phenotypic profiling features. Compounds/doses are filtered to include only 
+#' those that are bioactive in at least one cell line. The test set is generated
+#' by randomly sampling compounds (and taking all bioactive doses).
+#+ modeling, fig.height=8, fig.width=15, message=FALSE, warnings=FALSE, echo=FALSE
 ################################################################################
 # Modeling
 ################################################################################
-# (1) Run bioactivity - drop bioinactive
-# (2) Train multiclass RF on category (w/ holdout compounds) for each cell line
-# (3) Compare single cell accuracy (average, optimal) v. bagged
-# (4) RF distance for pathway, target
+xf <- mutate(x, Compound_Dose=str_c(Compound_ID, ', ', Dose_Category)) %>%
+  filter(Compound_Dose %in% xdist.select$Compound_Dose) %>%
+  select(!matches('^PC')) %>%
+  filter(Compound_Usage == 'reference_cpd')
+
+# Fit models for each cell line
+cell.lines <- unique(xf$Cell_Line)
+models <- lapply(cell.lines, function(cell.line) {
+  xc <- filter(xf, Cell_Line == cell.line)
+  
+  
+  # Fit model
+  xx <- select(xc, matches('^nonborder'))
+  yy <- as.factor(xc$Compound_Category)
+  
+  # Set training set
+  id.train <- createDataPartition(yy, times=25, p=0.9)
+  
+  out <- lapply(1:length(id.train), function(i) {
+    
+    ii <- id.train[[i]]
+    xup <- upSample(xx[ii,], yy[ii])
+    ytrain <- xup$Class
+    xtrain <- select(xup, -Class)
+    
+    fit <- iRF(x=xtrain, 
+               y=ytrain, 
+               n.iter=1, 
+               type='ranger', 
+               n.core=n.core)
+  
+    # Evaluate model class predictions
+    yy <- as.numeric(yy) - 1
+    ypred.class <- predict(fit$rf.list, xx[-ii,])$predictions
+    
+    # Evaluate model probability predictions
+    ypred <- predict(fit$rf.list, xx[-ii,], predict.all=TRUE)$predictions
+    ypred <- sapply(sort(unique(yy)), function(cl) rowMeans(ypred == cl))
+    ypred <- lapply(1:nrow(ypred), function(k) ypred[k,])
+  
+    out <- select(xc, -matches('^nonborder'))[-ii,] %>%
+      mutate(YpredCl=ypred.class) %>%
+      mutate(Ypred=ypred) %>%
+      mutate(Ytrue=yy[-ii]) %>%
+      mutate(Rep=i)
+    
+    return(out)
+  })
+  
+  out <- rbindlist(out)
+})
+
+models <- rbindlist(models)
+
+# Group model predictions across cell lines
+model.bag <- lapply(unique(models$Compound_Dose), function(g) {
+  out <- filter(models, Compound_Dose == g) %>% 
+    select(Compound_ID, Ypred, Ytrue, Compound_Category)
+  
+  ypred <- colMeans(do.call(rbind, out$Ypred))
+  out <- data.table(Compound_ID=out$Compound_ID[1]) %>%
+    mutate(Ytrue=out$Ytrue[1]) %>%
+    mutate(Compound_Dose=g) %>%
+    mutate(Ypred=list(ypred)) %>%
+    mutate(Compound_Category=out$Compound_Category[1]) %>%
+    mutate(YpredCl=(which.max(ypred) - 1))
+  return(out)
+})
+
+model.bag <- rbindlist(model.bag)
+bagg.acc <- mean(model.bag$YpredCl == model.bag$Ytrue)
+
+# Accuracy by cell line
+group_by(models, Cell_Line) %>%
+  summarize(Accuracy=mean(YpredCl == Ytrue)) %>%
+  mutate(Accuracy=round(Accuracy, 2)) %>%
+  ggplot(aes(x=Cell_Line, y=Accuracy)) +
+  geom_bar(stat='identity', fill='#0088D1') +
+  geom_text(aes(x=Cell_Line, y=Accuracy + 0.02, label=Accuracy)) +
+  theme_bw() +
+  geom_hline(yintercept=bagg.acc, col='red') +
+  ylim(c(0, 1))
+
+# Accuracy by compound category
+xplot.cell <- group_by(models, Cell_Line, Compound_Category) %>%
+  summarize(Accuracy=mean(YpredCl == Ytrue))
+
+xplot.bag <- group_by(model.bag, Compound_Category) %>%
+  summarize(Accuracy=mean(YpredCl == Ytrue)) %>%
+  mutate(Cell_Line='Aggregate')
+
+rbind(xplot.cell, xplot.bag) %>%
+  mutate(Accuracy=round(Accuracy, 2)) %>%
+  mutate(Cell_Line=factor(Cell_Line, levels=c('Aggregate', 'A549', '786-0', 'OVCAR4'))) %>%
+  ggplot(aes(x=Cell_Line, y=Accuracy, fill=Compound_Category)) +
+  geom_bar(stat='identity', position='dodge') +
+  geom_text(aes(label=Accuracy), position=position_dodge(width=0.9), vjust=-0.02) +
+  theme_bw() +
+  ylim(0:1)
 
